3a281674c92c12662cc99d2c728d84b694732228
Scott <scott@paragonie.com>
2015-10-14 14:58:17 -0400

    Migrate/adopt phpunit script from paragonie/halite
    (Thanks to @DefuseSec for troubleshooting that one too!)

--- run-tests.sh
+++ run-tests.sh
@@ -1,17 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-clean=1 # Clean up?
+origdir=`pwd`
+cdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $origdir
+parentdir="$(dirname $cdir)"
+
+clean=0 # Clean up?
 gpg --fingerprint D8406D0D82947747293778314AA394086372C20A
 if [ $? -ne 0 ]; then
     echo -e "\033[33mDownloading PGP Public Key...\033[0m"
-    gpg --recv-keys D8406D0D82947747293778314AA394086372C20A
+    gpg  --keyserver pgp.mit.edu --recv-keys D8406D0D82947747293778314AA394086372C20A
     # Sebastian Bergmann <sb@sebastian-bergmann.de>
     gpg --fingerprint D8406D0D82947747293778314AA394086372C20A
     if [ $? -ne 0 ]; then
         echo -e "\033[31mCould not download PGP public key for verification\033[0m"
-        exit
+        exit 1
     fi
 fi
+
 if [ "$clean" -eq 1 ]; then
     # Let's clean them up, if they exist
     if [ -f phpunit.phar ]; then
@@ -25,10 +31,21 @@ fi
 # Let's grab the latest release and its signature
 if [ ! -f phpunit.phar ]; then
     wget https://phar.phpunit.de/phpunit.phar
+    if [ $? -ne 0 ]; then
+        echo "wget phpunit.phar was unsuccessful"
+        exit 1
+    fi
 fi
 if [ ! -f phpunit.phar.asc ]; then
     wget https://phar.phpunit.de/phpunit.phar.asc
+    if [ $? -ne 0 ]; then
+        echo "wget phpunit.phar.asc was unsuccessful"
+        exit 1
+    fi
 fi
+
+# What are the major/minor versions?
+# php -r "var_dump([\Sodium\library_version_major(), \Sodium\library_version_minor()]);"
 
 # Verify before running
 gpg --verify phpunit.phar.asc phpunit.phar
@@ -36,13 +53,15 @@ if [ $? -eq 0 ]; then
     echo
     echo -e "\033[33mBegin Unit Testing\033[0m"
     # Run the testing suite
-    php phpunit.phar --bootstrap autoload.php tests
+    php phpunit.phar --bootstrap "$parentdir/autoload.php" "$parentdir/tests"
+    EXITCODE=$?
     # Cleanup
     if [ "$clean" -eq 1 ]; then
         echo -e "\033[32mCleaning Up!\033[0m"
         rm -f phpunit.phar
         rm -f phpunit.phar.asc
     fi
+    exit $EXITCODE
 else
     echo
     chmod -x phpunit.phar
