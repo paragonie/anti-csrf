11f7c0bc11ba4a3b1f55c5badfd6963b10a824c1
Scott <scott@paragonie.com>
2016-01-09 16:04:58 -0500

    Copyleft year

--- src/AntiCSRF.php
+++ src/AntiCSRF.php
@@ -2,7 +2,7 @@
 namespace ParagonIE\AntiCSRF;
 
 /**
- * Copyright (c) 2015 Paragon Initiative Enterprises <https://paragonie.com>
+ * Copyright (c) 2015 - 2016 Paragon Initiative Enterprises <https://paragonie.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
@@ -21,7 +21,7 @@ namespace ParagonIE\AntiCSRF;
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2015 Paragon Initiative Enterprises
+ * Copyright (c) 2015 - 2016 Paragon Initiative Enterprises
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
